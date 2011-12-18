b083b496899ad0c62a734ac28a36f6a243848137
(no author) <(no author)@bfc99d43-bc42-0410-8496-372ce3d59e05>
2011-12-18 13:13:52 +0000

    updated translations

--- trunk/tabletReader_fr.ts
+++ trunk/tabletReader_fr.ts
@@ -4,25 +4,24 @@
 <context>
     <name>FileBrowserModel</name>
     <message>
-        <location filename="filebrowsermodel.cpp" line="44"/>
-        <location filename="filebrowsermodel.cpp" line="133"/>
+        <location filename="filebrowsermodel.cpp" line="43"/>
+        <location filename="filebrowsermodel.cpp" line="131"/>
         <source>Go</source>
         <translation>Retourner</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="83"/>
+        <location filename="filebrowsermodel.cpp" line="81"/>
         <source>Go Back To &apos;</source>
         <translation>Retourner à &apos;</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="85"/>
+        <location filename="filebrowsermodel.cpp" line="83"/>
         <source>Go Back To /</source>
         <translation>Retourner à /</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="153"/>
         <source>Current folder: </source>
-        <translation>Répertoire courant: </translation>
+        <translation type="obsolete">Répertoire courant: </translation>
     </message>
 </context>
 <context>
@@ -101,8 +100,12 @@
     </message>
     <message>
         <location filename="qml/PopupMenu/PopupListModel.qml" line="32"/>
+        <source>Properties</source>
+        <translation>Propriétés</translation>
+    </message>
+    <message>
         <source>Show/Hide Page Number</source>
-        <translation>Afficher/Masquer le Nombre de la Page</translation>
+        <translation type="obsolete">Afficher/Masquer le Nombre de la Page</translation>
     </message>
     <message>
         <location filename="qml/PopupMenu/PopupListModel.qml" line="33"/>
@@ -113,22 +116,22 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="window.cpp" line="83"/>
+        <location filename="window.cpp" line="81"/>
         <source>Ctrl++</source>
         <translation>Ctrl++</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="85"/>
+        <location filename="window.cpp" line="83"/>
         <source>Ctrl+-</source>
         <translation>Ctrl+-</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="857"/>
+        <location filename="window.cpp" line="829"/>
         <source>&lt;H2&gt;tabletReader v1.1&lt;/H2&gt;&lt;br&gt;&lt;H3&gt;PDF viewer for touch-enabled devices&lt;/H3&gt;&lt;br&gt;Copyright (C) 2011, Bogdan Cristea. All rights reserved.&lt;br&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;br&gt;&lt;br&gt;</source>
         <translation>&lt;H2&gt; TabletReader v1.1 &lt;/ h2&gt; &lt;br&gt; &lt;H3&gt; lecteur PDF pour écrans tactiles &lt;/ H3&gt; &lt;br&gt; Copyright (C) 2011, Bogdan Cristea. Tous droits réservés. &lt;br&gt; Ce programme est distribué dans l&apos;espoir qu&apos;il sera utile, mais SANS AUCUNE GARANTIE, sans même la garantie implicite de COMMERCIALISATION ou D&apos;ADAPTATION A UN OBJET PARTICULIER. Voir la Licence Publique Générale GNU pour plus de détails. &lt;br&gt; &lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="966"/>
+        <location filename="window.cpp" line="938"/>
         <source>Cannot get authorization code for Intel AppUp(TM) software</source>
         <translation>Impossible de lire le code d&apos;autorisation pour le logiciel Intel AppUp (TM)</translation>
     </message>
@@ -137,91 +140,105 @@
         <translation type="obsolete">Vous ne pouvez pas utiliser tabletReader</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="209"/>
+        <location filename="window.cpp" line="201"/>
         <source>Full Screen</source>
         <translation>Plein Ecran</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="212"/>
-        <location filename="window.cpp" line="469"/>
+        <location filename="window.cpp" line="204"/>
+        <location filename="window.cpp" line="464"/>
         <source>Go To Page</source>
         <translation>Aller à la Page</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="215"/>
-        <location filename="window.cpp" line="475"/>
+        <location filename="window.cpp" line="207"/>
+        <location filename="window.cpp" line="470"/>
         <source>Zoom</source>
         <translation>Agrandir</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="218"/>
+        <location filename="window.cpp" line="210"/>
+        <location filename="window.cpp" line="467"/>
+        <source>Properties</source>
+        <translation>Propriétés</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="213"/>
         <source>Help</source>
         <translation>Aide</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="221"/>
+        <location filename="window.cpp" line="216"/>
         <source>About</source>
         <translation>A Propos</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="224"/>
-        <location filename="window.cpp" line="478"/>
+        <location filename="window.cpp" line="219"/>
+        <location filename="window.cpp" line="473"/>
         <source>Exit</source>
         <translation>Sortie</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="240"/>
+        <location filename="window.cpp" line="235"/>
         <source>Cannot create fileBrowser object</source>
         <translation>Impossible de créer l&apos;objet fileBrowser</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="241"/>
+        <location filename="window.cpp" line="236"/>
         <source>not enough memory</source>
         <translation>mémoire insuffissante</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="246"/>
+        <location filename="window.cpp" line="241"/>
         <source>fileBrowserObject is NULL</source>
         <translation>l&apos;objet fileBrowserObject est NULL</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="967"/>
+        <location filename="window.cpp" line="939"/>
         <source>You can use tabletReader, but it is highly recommended to connect to Intel AppUp center</source>
         <translation>Vous pouvez utiliser tabletReader, mais il est fortement recommandé d&apos;être connecté au centre AppUp d&apos;Intel</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="206"/>
-        <location filename="window.cpp" line="466"/>
+        <location filename="window.cpp" line="965"/>
+        <source>&lt;H3&gt;Document path:&lt;br&gt;&lt;i&gt;%1&lt;/i&gt;&lt;/H3&gt;</source>
+        <translation>&lt;H3&gt;Chemin du document:&lt;br&gt;&lt;i&gt;%1&lt;/i&gt;&lt;/H3&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="967"/>
+        <source>&lt;H3&gt;Current page / Page number:&lt;br&gt;&lt;b&gt;%1 / %2&lt;/b&gt;&lt;/H3&gt;</source>
+        <translation>&lt;H3&gt;Page courante / Nombre de pages:&lt;br&gt;&lt;b&gt;%1 / %2&lt;/b&gt;&lt;/H3&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="198"/>
+        <location filename="window.cpp" line="461"/>
         <source>Open</source>
         <translation>Ouvrir</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="472"/>
         <source>Show/Hide Page Number</source>
-        <translation>Afficher/Masquer le Nombre de la Page</translation>
+        <translation type="obsolete">Afficher/Masquer le Nombre de la Page</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="481"/>
+        <location filename="window.cpp" line="476"/>
         <source>Normal Screen</source>
         <translation>Ecran Normal</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="504"/>
+        <location filename="window.cpp" line="499"/>
         <source>Failed to open file</source>
         <translation>Impossible d&apos;ouvrir le fichier</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="505"/>
+        <location filename="window.cpp" line="500"/>
         <source>The specified file cannot be opened</source>
         <translation>Le fichier spécifié ne peut pas être ouvert</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="709"/>
         <source>page %1 of %2</source>
-        <translation>page %1 sur %2</translation>
+        <translation type="obsolete">page %1 sur %2</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="833"/>
+        <location filename="window.cpp" line="805"/>
         <source>Cannot open help file</source>
         <translation>Impossible d&apos;ouvrir le fichier d&apos;aide</translation>
     </message>
@@ -262,16 +279,21 @@
     </message>
     <message>
         <location filename="qml/maintoolbar.qml" line="56"/>
+        <source>Properties</source>
+        <translation>Propriétés</translation>
+    </message>
+    <message>
+        <location filename="qml/maintoolbar.qml" line="62"/>
         <source>Help</source>
         <translation>Aide</translation>
     </message>
     <message>
-        <location filename="qml/maintoolbar.qml" line="62"/>
+        <location filename="qml/maintoolbar.qml" line="68"/>
         <source>About</source>
         <translation>A Propos</translation>
     </message>
     <message>
-        <location filename="qml/maintoolbar.qml" line="68"/>
+        <location filename="qml/maintoolbar.qml" line="74"/>
         <source>Exit</source>
         <translation>Sortie</translation>
     </message>
--- trunk/tabletReader_ro.ts
+++ trunk/tabletReader_ro.ts
@@ -4,25 +4,24 @@
 <context>
     <name>FileBrowserModel</name>
     <message>
-        <location filename="filebrowsermodel.cpp" line="44"/>
-        <location filename="filebrowsermodel.cpp" line="133"/>
+        <location filename="filebrowsermodel.cpp" line="43"/>
+        <location filename="filebrowsermodel.cpp" line="131"/>
         <source>Go</source>
         <translation>Revenire</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="83"/>
+        <location filename="filebrowsermodel.cpp" line="81"/>
         <source>Go Back To &apos;</source>
         <translation>Revenire la &apos;</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="85"/>
+        <location filename="filebrowsermodel.cpp" line="83"/>
         <source>Go Back To /</source>
         <translation>Revenire la /</translation>
     </message>
     <message>
-        <location filename="filebrowsermodel.cpp" line="153"/>
         <source>Current folder: </source>
-        <translation>Director curent: </translation>
+        <translation type="obsolete">Director curent: </translation>
     </message>
 </context>
 <context>
@@ -101,8 +100,12 @@
     </message>
     <message>
         <location filename="qml/PopupMenu/PopupListModel.qml" line="32"/>
+        <source>Properties</source>
+        <translation>Proprietati</translation>
+    </message>
+    <message>
         <source>Show/Hide Page Number</source>
-        <translation>Afiseaza/Ascunde Numarul de Pagina</translation>
+        <translation type="obsolete">Afiseaza/Ascunde Numarul de Pagina</translation>
     </message>
     <message>
         <location filename="qml/PopupMenu/PopupListModel.qml" line="33"/>
@@ -113,22 +116,22 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="window.cpp" line="83"/>
+        <location filename="window.cpp" line="81"/>
         <source>Ctrl++</source>
         <translation>Ctrl++</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="85"/>
+        <location filename="window.cpp" line="83"/>
         <source>Ctrl+-</source>
         <translation>Ctrl+-</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="857"/>
+        <location filename="window.cpp" line="829"/>
         <source>&lt;H2&gt;tabletReader v1.1&lt;/H2&gt;&lt;br&gt;&lt;H3&gt;PDF viewer for touch-enabled devices&lt;/H3&gt;&lt;br&gt;Copyright (C) 2011, Bogdan Cristea. All rights reserved.&lt;br&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;br&gt;&lt;br&gt;</source>
         <translation>&lt;H2&gt; TabletReader v1.1 &lt;/ H2&gt; &lt;br&gt; &lt;h3&gt; cititor PDF pentru dispozitivele cu ecran tactil &lt;/ H3&gt; &lt;br&gt; Copyright (C) 2011, Bogdan Cristea. Toate drepturile rezervate. &lt;br&gt; Acest program este distribuit în speranţa că va fi util, fără nici o GARANŢIE, nici chiar fără garanţia implicită de vandabilitate sau POTRIVIRE PENTRU UN ANUMIT SCOP. A se vedea GNU General Public License pentru mai multe detalii. &lt;br&gt; &lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="966"/>
+        <location filename="window.cpp" line="938"/>
         <source>Cannot get authorization code for Intel AppUp(TM) software</source>
         <translation>Nu se poate citi codul de autorizare pentru programul Intel AppUp (TM)</translation>
     </message>
@@ -137,91 +140,105 @@
         <translation type="obsolete">Nu puteti folosi tabletReader</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="209"/>
+        <location filename="window.cpp" line="201"/>
         <source>Full Screen</source>
         <translation>Tot Ecranul</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="212"/>
-        <location filename="window.cpp" line="469"/>
+        <location filename="window.cpp" line="204"/>
+        <location filename="window.cpp" line="464"/>
         <source>Go To Page</source>
         <translation>Mergi la Pagina</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="215"/>
-        <location filename="window.cpp" line="475"/>
+        <location filename="window.cpp" line="207"/>
+        <location filename="window.cpp" line="470"/>
         <source>Zoom</source>
         <translation>Marire</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="218"/>
+        <location filename="window.cpp" line="210"/>
+        <location filename="window.cpp" line="467"/>
+        <source>Properties</source>
+        <translation>Proprietati</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="213"/>
         <source>Help</source>
         <translation>Ajutor</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="221"/>
+        <location filename="window.cpp" line="216"/>
         <source>About</source>
         <translation>Despre</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="224"/>
-        <location filename="window.cpp" line="478"/>
+        <location filename="window.cpp" line="219"/>
+        <location filename="window.cpp" line="473"/>
         <source>Exit</source>
         <translation>Iesire</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="240"/>
+        <location filename="window.cpp" line="235"/>
         <source>Cannot create fileBrowser object</source>
         <translation>Nu se poate crea obiectul fileBrowser</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="241"/>
+        <location filename="window.cpp" line="236"/>
         <source>not enough memory</source>
         <translation>memorie insuficienta</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="246"/>
+        <location filename="window.cpp" line="241"/>
         <source>fileBrowserObject is NULL</source>
         <translation>obiectul fileBrowserObject este NULL</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="967"/>
+        <location filename="window.cpp" line="939"/>
         <source>You can use tabletReader, but it is highly recommended to connect to Intel AppUp center</source>
         <translation>Puteti folosi tabletReader, dar este recomandat sa fiti conectat la centrul AppUp al Intel</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="206"/>
-        <location filename="window.cpp" line="466"/>
+        <location filename="window.cpp" line="965"/>
+        <source>&lt;H3&gt;Document path:&lt;br&gt;&lt;i&gt;%1&lt;/i&gt;&lt;/H3&gt;</source>
+        <translation>&lt;H3&gt;Calea documentului:&lt;br&gt;&lt;i&gt;%1&lt;/i&gt;&lt;/H3&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="967"/>
+        <source>&lt;H3&gt;Current page / Page number:&lt;br&gt;&lt;b&gt;%1 / %2&lt;/b&gt;&lt;/H3&gt;</source>
+        <translation>&lt;H3&gt;Pagina curenta / Numarul de pagini:&lt;br&gt;&lt;b&gt;%1 / %2&lt;/b&gt;&lt;/H3&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="198"/>
+        <location filename="window.cpp" line="461"/>
         <source>Open</source>
         <translation>Deschide</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="472"/>
         <source>Show/Hide Page Number</source>
-        <translation>Afiseaza/Ascunde Numarul de Pagina</translation>
+        <translation type="obsolete">Afiseaza/Ascunde Numarul de Pagina</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="481"/>
+        <location filename="window.cpp" line="476"/>
         <source>Normal Screen</source>
         <translation>Ecran Normal</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="504"/>
+        <location filename="window.cpp" line="499"/>
         <source>Failed to open file</source>
         <translation>Fisierul nu poate fi deschis</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="505"/>
+        <location filename="window.cpp" line="500"/>
         <source>The specified file cannot be opened</source>
         <translation>Fisierul specificat nu poate fi deschis</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="709"/>
         <source>page %1 of %2</source>
-        <translation>pagina %1 din %2</translation>
+        <translation type="obsolete">pagina %1 din %2</translation>
     </message>
     <message>
-        <location filename="window.cpp" line="833"/>
+        <location filename="window.cpp" line="805"/>
         <source>Cannot open help file</source>
         <translation>Nu se poate deschide fichierul de ajutor</translation>
     </message>
@@ -262,16 +279,21 @@
     </message>
     <message>
         <location filename="qml/maintoolbar.qml" line="56"/>
+        <source>Properties</source>
+        <translation>Proprietati</translation>
+    </message>
+    <message>
+        <location filename="qml/maintoolbar.qml" line="62"/>
         <source>Help</source>
         <translation>Ajutor</translation>
     </message>
     <message>
-        <location filename="qml/maintoolbar.qml" line="62"/>
+        <location filename="qml/maintoolbar.qml" line="68"/>
         <source>About</source>
         <translation>Despre</translation>
     </message>
     <message>
-        <location filename="qml/maintoolbar.qml" line="68"/>
+        <location filename="qml/maintoolbar.qml" line="74"/>
         <source>Exit</source>
         <translation>Iesire</translation>
     </message>
