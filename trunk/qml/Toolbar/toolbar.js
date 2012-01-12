ee4d8ff7b2c1dbe2df282972cc6ebf272709297b
(no author) <(no author)@bfc99d43-bc42-0410-8496-372ce3d59e05>
2012-01-12 11:34:58 +0000

    corrected function for toolbar button width computation

--- trunk/qml/Toolbar/toolbar.js
+++ trunk/qml/Toolbar/toolbar.js
@@ -3,24 +3,11 @@
 // Calculate item width to fill the width of the container while maintaining
 // minimum item width
 function calcTabWidth() {
+    var margin = 1
     var containerWidth = parent.width
     var itemCount = views.length
     var itemWidth = Math.floor(containerWidth / itemCount) >= minimumItemWidth ?
             Math.floor(containerWidth / itemCount) : minimumItemWidth;
-
-    // If content of a tab is wider than the global tab width, the tab
-    // will use more space
-    for (var i = 0; i < views.length; ++i) {
-        if (views[i].contentWidth > itemWidth) {
-            containerWidth -= content[i].contentWidth;
-            tabCount--;
-        }
-    }
-
-    if (itemCount > 0) {
-        itemWidth = Math.floor(containerWidth / itemCount) >= minimumItemWidth ?
-                    Math.floor(containerWidth / itemCount) : minimumItemWidth;
-    }
-
+    itemWidth -= margin //needed in order to display completely all buttons
     return itemWidth;
 }
